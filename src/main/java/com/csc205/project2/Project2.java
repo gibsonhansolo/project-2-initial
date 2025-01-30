6937ff0c94868e0e3e4d45a8d18b3c5fec37779b
gibsonhansolo <gib2151201@maricopa.edu>
2025-01-30 10:27:50 -0700

    add cone class to driver class

--- src/main/java/com/csc205/project2/Project2.java
+++ src/main/java/com/csc205/project2/Project2.java
@@ -17,17 +17,18 @@ public class Project2 {
         The following code works with the example design given.
          */
 
-        /*ThreeDimensionalShape sphere = new Sphere(2.0);
+        ThreeDimensionalShape sphere = new Sphere(2.0);
+        ThreeDimensionalShape cone = new Cone(6.0, 4.0);
         ThreeDimensionalShape cube = new Cube(5.0);
         ThreeDimensionalShape cylinder = new Cylinder(4.0, 1.0);
 
         // We'll cover this stuff in module 4
         List<ThreeDimensionalShape> shapes = new ArrayList<>();
         shapes.add(sphere);
+        shapes.add(cone);
         shapes.add(cube);
         shapes.add(cylinder);
-
-        shapes.forEach(System.out::println);*/
+        shapes.forEach(System.out::println);
 
         /*
         The following code works with the code given.
