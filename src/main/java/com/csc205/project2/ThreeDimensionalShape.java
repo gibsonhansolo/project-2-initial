c0a304fb52fe1fe012eebc533edea29e01009ca9
gibsonhansolo <gib2151201@maricopa.edu>
2025-01-30 10:22:18 -0700

    Implement ThreeDimensionalShape Abstract Class

---
+++ src/main/java/com/csc205/project2/ThreeDimensionalShape.java
@@ -0,0 +1,11 @@
+package com.csc205.project2;
+
+/**
+ *  Prompt used with ClaudeAi
+ *  Generate the following java program based on the UML diagram.
+ */
+
+abstract class ThreeDimensionalShape implements Shape {
+    public abstract double volume();
+    public abstract double surfaceArea();
+}
